3db6bbba883d81840ccf4bfe56c73eadaca38e93
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-06 17:52:45 -0400

    chore: minutes passed Component

---
+++ src/components/MinutesPassed.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export interface MinutesPassedProps {
+    from: Date;
+}
+
+export default function MinutesPassed({
+    from = new Date(),
+}: MinutesPassedProps) {
+    const [minutes, setMinutes] = useState(0);
+
+    useEffect(() => {
+        function update() {
+            const diff = Math.floor((new Date().getTime() - from.getTime()) / 60000);
+            setMinutes(diff);
+        }
+
+        update();
+        const interval = setInterval(update, 60000);
+
+        return () => clearInterval(interval);
+    }, [from]);
+
+    return <>{minutes}</>;
+}
