91acbaa81e55973c4025d6424c6f381d12db5b38
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-07 18:42:41 -0400

    feat: input

--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
-const InputStyle = styled.input``;
+const InputStyle = styled.input<{ height?: string; width?: string }>`
+  height: ${(props) => props.height || "24px"};
+  width: ${(props) => props.width || "184px"};
+  background-color: transparent;
+  border-radius: 8px;
+  outline: none;
+  border: none;
+  font-size: 15px;
+  line-height: 24px;
+`;
 
 type InputProps = React.ComponentProps<typeof InputStyle>;
 
