11c9f97e060c8c5d38020ed3a8282059ca0e582b
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-06 23:55:54 -0400

    chore: dashboard layout and page

---
+++ src/app/dashboard/[slug]/page.tsx
@@ -0,0 +1,22 @@
+import { notFound } from "next/navigation";
+
+const validSlugs = ["ordersheets", "areas"] as const;
+const validSlugsSet = new Set(validSlugs);
+
+export function generateStaticParams() {
+    return validSlugs.map((slug) => ({ slug }));
+}
+
+interface DashboardProps {
+    params: Promise<{ slug: (typeof validSlugs)[number] }>;
+}
+
+export default async function Dashboard({ params }: DashboardProps) {
+    const { slug } = await params;
+
+    if (!validSlugsSet.has(slug)) {
+        notFound();
+    }
+
+    return <div>Conteúdo da página: {slug}</div>;
+}
--- src/app/dashboard/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function OrdersheetsDashboard() {
-    return <>test</>;
-}
