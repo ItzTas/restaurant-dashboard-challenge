38165e77cabe5df402c94b5dbd4581f2bc66ac2d
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-07 21:19:53 -0400

    chore: icons renamed

--- src/components/icons/RoomServiceIcon.tsx
+++ src/components/icons/RoomServiceIcon.tsx
@@ -1,4 +1,4 @@
-export default function StatusIcon(props: React.SVGProps<SVGSVGElement>) {
+export default function RoomServiceIcon(props: React.SVGProps<SVGSVGElement>) {
     return (
         <svg
             {...props}
--- src/components/icons/iconPresets.ts
+++
@@ -1,9 +0,0 @@
-import { SVGProps } from "react";
-
-export const descriptionIconProps: SVGProps<SVGSVGElement> = {
-    width: "10px",
-    height: "10px",
-    style: {
-        marginRight: "2px",
-    },
-};
