14112a6e9f32741314e673a49d594b32aa033c79
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-07 22:54:29 -0400

    chore: dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,10 +1,42 @@
 import { PropsWithChildren } from "react";
+import styled from "styled-components";
 import DashboardContextProvider from "./DashboardContext";
+import DashboardTopbar from "@/components/bars/DashboardTopbar";
+import DashboardSidebar from "@/components/bars/DashboardSidebar";
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+`;
+
+const SidebarWrapper = styled(DashboardSidebar)`
+  grid-column: 1;
+  grid-row: 1 / -1;
+`;
+
+const TopbarWrapper = styled(DashboardTopbar)`
+  grid-column: 2;
+  grid-row: 1;
+`;
+
+const MainContent = styled.main`
+  grid-column: 2;
+  grid-row: 2;
+  overflow-y: scroll;
+`;
 
 export default function DashboardLayout({ children }: PropsWithChildren) {
     return (
         <DashboardContextProvider>
-            <div>{children}</div>
+            <GridContainer>
+                <SidebarWrapper />
+                <TopbarWrapper />
+                <MainContent>{children}</MainContent>
+            </GridContainer>
         </DashboardContextProvider>
     );
 }
