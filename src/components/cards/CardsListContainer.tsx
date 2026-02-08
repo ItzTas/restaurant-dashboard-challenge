1b7ff13f4887dae4fc22c60e13f4f84948132f78
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-08 17:17:34 -0400

    chore: cards list container

---
+++ src/components/cards/CardsListContainer.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const CardsListContainer = styled.div`
+  gap: 12px;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 199px);
+  justify-content: center;
+
+  @media (min-width: 768px) {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+  }
+`;
+
+export default CardsListContainer;
