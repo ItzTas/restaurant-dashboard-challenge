d1b7edde966405dfc50a4d6c708f1c91a9576e3e
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-07 21:19:13 -0400

    chore: dashboard slugs array

--- src/constants/dashboard.ts
+++ src/constants/dashboard.ts
@@ -2,3 +2,7 @@ export const DashboardValidSlugs = {
     Ordersheets: "ordersheets",
     Areas: "areas",
 } as const;
+
+export const DashboardValidSlugsArray = Object.freeze(
+    Object.values(DashboardValidSlugs),
+) as readonly (typeof DashboardValidSlugs)[keyof typeof DashboardValidSlugs][];
