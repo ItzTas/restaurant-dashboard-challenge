a98059ac705f45487a3136f252d86edc13e25313
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-08 03:01:13 -0400

    chore: dashboard topbar

--- src/components/bars/DashboardTopbar.tsx
+++ src/components/bars/DashboardTopbar.tsx
@@ -1,9 +1,30 @@
 import styled from "styled-components";
+import SearchInput from "../inputs/SearchInput";
+import NavOptions, { TabItem } from "../buttons/NavOptions";
+import { DashboardValidSlugsArray } from "@/constants/dashboard";
 
 const NavContainer = styled.nav`
   padding: 12px 16px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const RightContainer = styled.div`
+  display: flex;
 `;
 
 export default function DashboardTopbar() {
-    return <NavContainer>Topbar</NavContainer>;
+    const tabs: TabItem[] = DashboardValidSlugsArray.map((slug) => ({
+        label: slug,
+        path: `${slug}`,
+    }));
+
+    return (
+        <NavContainer>
+            <RightContainer>
+                <NavOptions tabs={tabs} />
+                <SearchInput />
+            </RightContainer>
+        </NavContainer>
+    );
 }
