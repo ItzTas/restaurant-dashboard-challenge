8dc01dd7783540a047003bf51eec97420b22e81d
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-07 21:19:44 -0400

    chore: search input icon

--- src/components/inputs/SearchInput.tsx
+++ src/components/inputs/SearchInput.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Input from "./Input";
+import SearchIcon from "../icons/SearchIcon";
 
 type SearchInputProps = React.ComponentProps<typeof Input>;
 
 export default function SearchInput({ ...props }: SearchInputProps) {
-    return <Input {...props} />;
+    return (
+        <label>
+            <SearchIcon width={17} height={17} />
+            <Input {...props} />
+        </label>
+    );
 }
