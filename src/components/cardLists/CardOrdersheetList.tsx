8727785129c44b2dd5a77e6a29b9d88100fdb652
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-09 17:23:53 -0400

    chore: prop rename

--- src/components/cardLists/CardOrdersheetList.tsx
+++ src/components/cardLists/CardOrdersheetList.tsx
@@ -4,11 +4,11 @@ import CardOrdersheet from "../cards/CardOrdersheet";
 import { CardOrdersheetProps } from "../cards/CardOrdersheet";
 import CardsGridList from "./CardsGridList";
 
-interface Props {
+interface CardOrdersheetListProps {
     cards: CardOrdersheetProps[];
 }
 
-export default function CardOrdersheetList({ cards }: Props) {
+export default function CardOrdersheetList({ cards }: CardOrdersheetListProps) {
     return (
         <CardsGridList
             data={cards}
