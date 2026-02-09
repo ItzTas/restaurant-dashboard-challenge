2f57372bee7a2c16bd1337760eab84b106e4bf10
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-09 17:24:21 -0400

    chore: cards mesa list uses card grid list

--- src/components/cardLists/CardMesaList.tsx
+++ src/components/cardLists/CardMesaList.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import CardMesa from "../cards/CardMesa";
 import { CardMesaProps } from "../cards/CardMesa";
+import CardsGridList from "./CardsGridList";
 
 export interface CardMesaListProps {
     cards: CardMesaProps[];
 }
 
-export default function CardMesaList({ }: CardMesaProps) { }
+export default function CardMesaList({ cards }: CardMesaListProps) {
+    return (
+        <CardsGridList
+            data={cards}
+            itemContent={(_, card) => <CardMesa {...card} />}
+        />
+    );
+}
