aa9777f68c6b96ea7bd0b1a95c6d039068d9c8b0
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-10 18:31:21 -0400

    chore: filters slice added

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import filtersReducer from "@/features/filters/slice";
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        filters: filtersReducer,
+    },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
