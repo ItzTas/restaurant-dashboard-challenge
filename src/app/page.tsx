9d8288cb2455c810cddf27000674b5e0c310cf02
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-10 10:58:23 -0400

    chore: test lines removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,3 @@
-"use client";
-
-import DropdownPrimary from "@/components/dropdowns/DropdownPrimary";
-import { useState } from "react";
-
 export default function Home() {
-    const [selectedArea, setSelectedArea] = useState("visao-geral");
-
-    const areas = [
-        { id: "visao-geral", label: "Visão Geral", subtitle: "todas as áreas" },
-        { id: "salao-principal", label: "Salão Principal" },
-        { id: "lounge", label: "Lounge" },
-        { id: "area-externa", label: "Área Externa" },
-    ];
-
-    return (
-        <div style={{ padding: "40px" }}>
-            <DropdownPrimary
-                options={areas}
-                defaultValue={"visao-geral"}
-                onChange={(value) => setSelectedArea(value)}
-            />
-        </div>
-    );
+    return <div style={{ padding: "40px" }}></div>;
 }
