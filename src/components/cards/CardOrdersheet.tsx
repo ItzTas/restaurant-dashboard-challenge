55040c4f59fbe7c33b4debdd762268d48224794d
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-10 15:17:40 -0400

    chore: contact interface created

--- src/components/cards/CardOrdersheet.tsx
+++ src/components/cards/CardOrdersheet.tsx
@@ -3,7 +3,6 @@ import React, { cloneElement, JSX } from "react";
 import CallIcon from "../icons/CallIcon";
 import AvatarIcon from "../icons/AvatarIcon";
 import TableIcon from "../icons/TableIcon";
-import LocationIcon from "../icons/LocationIcon";
 import CardContainer from "./CardContainer";
 import CardHeader from "./CardHeader";
 import CardDescription from "./CardDescription";
@@ -14,40 +13,49 @@ import {
 import CardSummaryFooter from "./CardSummaryFooter";
 import CardDescriptionRow from "./CardDescriptionRow";
 import { getInitials } from "@/utils/string";
+import CustomersIcon from "../icons/CustomersIcon";
+import { Model } from "@/types/model";
+import CheckpadIcon from "../icons/CheckpadIcon";
+
+export interface CardOrdersheetContact {
+    value: string;
+    type: "phone" | "customer";
+}
 
 export interface CardOrdersheetProps {
     identifier: string;
-    subtitle?: {
-        value: string;
-        type: "phone" | "id" | "customer";
-    };
+    contact?: CardOrdersheetContact;
+    customersNum?: number;
     tableText?: string;
-    locationText?: string;
+    model?: Model;
     idleTime: number;
     waiterFullName: string;
     totalPrice: number;
 }
 
-type SubtitleType = NonNullable<CardOrdersheetProps["subtitle"]>["type"];
+type SubtitleType = NonNullable<CardOrdersheetProps["contact"]>["type"];
 
 export default function CardOrdersheet({
     identifier,
-    subtitle,
+    contact,
+    customersNum,
     tableText,
-    locationText,
+    model,
     idleTime,
     waiterFullName,
     totalPrice,
     ...props
 }: CardOrdersheetProps & React.ComponentProps<typeof CardContainer>) {
     function getSubtitleIcon() {
+        if (!contact?.type) {
+            return;
+        }
         const icons: Record<SubtitleType, JSX.Element> = {
             phone: <CallIcon />,
-            id: <ReceiptIcon />,
             customer: <AvatarIcon />,
         } as const;
 
-        return cloneElement(icons[subtitle!.type], descriptionIconProps);
+        return cloneElement(icons[contact.type], descriptionIconProps);
     }
 
     const waiter = waiterFullName ? getInitials(waiterFullName) : "--";
@@ -60,12 +68,12 @@ export default function CardOrdersheet({
                     {identifier}
                 </CardHeader>
 
-                {(subtitle || tableText || locationText) && (
+                {(contact || tableText || model) && (
                     <CardDescription $mt="4px">
-                        {subtitle && (
+                        {contact && (
                             <CardDescriptionRow>
                                 {getSubtitleIcon()}
-                                <span>{subtitle.value}</span>
+                                <span>{contact.value}</span>
                             </CardDescriptionRow>
                         )}
                         {tableText && (
@@ -74,10 +82,16 @@ export default function CardOrdersheet({
                                 <span>{tableText}</span>
                             </CardDescriptionRow>
                         )}
-                        {locationText && (
+                        {model && (
                             <CardDescriptionRow>
-                                <LocationIcon {...descriptionIconProps} />
-                                <span>{locationText}</span>
+                                <CheckpadIcon icon={model.icon} {...descriptionIconProps} />
+                                <span>{model.value}</span>
+                            </CardDescriptionRow>
+                        )}
+                        {customersNum && (
+                            <CardDescriptionRow>
+                                <CustomersIcon {...descriptionIconProps} />
+                                {customersNum}
                             </CardDescriptionRow>
                         )}
                     </CardDescription>
