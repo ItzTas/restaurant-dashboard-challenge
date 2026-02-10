eea34181b0f07892d2cf92365d00502ada453f96
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-10 15:18:56 -0400

    chore: get checkpad records removed

--- src/lib/api/checkpadResponse.ts
+++ src/lib/api/checkpadResponse.ts
@@ -16,29 +16,3 @@ export async function getCheckpadById(id: string | number) {
     }
     return res.json();
 }
-
-export async function getCheckpadsRecordByIds(
-    ids: number[],
-): Promise<Record<string, CheckpadValue>> {
-    if (ids.length === 0) {
-        return {};
-    }
-
-    const query = ids.map((id) => `id=${id}`).join("&");
-
-    const res = await fetch(`${apiUrl}/checkpadsResponse?${query}`);
-
-    if (!res.ok) {
-        throw new Error("Failed to fetch checkpads");
-    }
-
-    const checkpadsArray: CheckpadValue[] = await res.json();
-
-    const checkpadsObject: Record<string, CheckpadValue> = {};
-
-    for (const checkpad of checkpadsArray) {
-        checkpadsObject[checkpad.id] = checkpad;
-    }
-
-    return checkpadsObject;
-}
