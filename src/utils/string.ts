ddb1e3dfe7448682c9e93ecb93bc2be4e654afa8
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-09 23:40:24 -0400

    chore: get initials

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,3 +1,10 @@
 export function capitalize(str: string) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export function getInitials(name: string): string {
+    return name
+        .split(" ")
+        .map((part) => part[0].toUpperCase())
+        .join("");
+}
