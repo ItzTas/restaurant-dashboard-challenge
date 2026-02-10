4c354c7a5ea274ea3878eca9d955b7f7404eb6fb
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-09 21:01:24 -0400

    chore: get checkpads record by ids api call

--- src/lib/api/checkpadResponse.ts
+++ src/lib/api/checkpadResponse.ts
@@ -1,4 +1,4 @@
-import { CheckpadsResponse } from "@/types/apiResponse.types";
+import { CheckpadsResponse, CheckpadValue } from "@/types/apiResponse.types";
 import { apiUrl } from "../../constants/api";
 
 export async function getAllCheckpads(): Promise<CheckpadsResponse> {
@@ -7,4 +7,30 @@ export async function getAllCheckpads(): Promise<CheckpadsResponse> {
         throw new Error("Failed to fetch");
     }
     return res.json();
+}
+
+export async function getCheckpadsRecordByIds(
+    ids: number[],
+): Promise<Record<string, CheckpadValue>> {
+    if (ids.length === 0) {
+        return {};
+    }
+
+    const query = ids.map((id) => `id=${id}`).join("&");
+
+    const res = await fetch(`${apiUrl}/checkpads?${query}`);
+
+    if (!res.ok) {
+        throw new Error("Failed to fetch checkpads");
+    }
+
+    const checkpadsArray: CheckpadValue[] = await res.json();
+
+    const checkpadsObject: Record<string, CheckpadValue> = {};
+
+    for (const checkpad of checkpadsArray) {
+        checkpadsObject[checkpad.id] = checkpad;
+    }
+
+    return checkpadsObject;
 }
