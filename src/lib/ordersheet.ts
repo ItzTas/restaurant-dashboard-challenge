8e9719d62ee764acb5111c5bc2362e65b08da2a9
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-10 14:11:59 -0400

    chore: get order sheets

--- src/lib/ordersheet.ts
+++ src/lib/ordersheet.ts
@@ -1,7 +1,43 @@
 import { CardOrdersheetProps } from "@/components/cards/CardOrdersheet";
+import { getAllOrdersheet } from "./api/ordersheet";
+import { OrdersheetValue } from "@/types/api.types";
+
+function getOrderIdentifier(val: OrdersheetValue): string {
+    const priorities: (keyof OrdersheetValue)[] = [
+        "mainIdentifier",
+        "customerName",
+        "contact",
+        "identifier",
+        "id",
+    ] as const;
+
+    for (const priority of priorities) {
+        if (val[priority]) {
+            return String(val[priority]);
+        }
+    }
+
+    return "";
+}
+
+export async function orderSheetToProps(
+    val: OrdersheetValue,
+): Promise<CardOrdersheetProps> {
+    const identifier = getOrderIdentifier(val);
+
+    return {
+        identifier,
+        idleTime: val.idleTime,
+    };
+}
 
 export async function getDashboardOrdersheets(): Promise<
     CardOrdersheetProps[]
 > {
-    return [];
+    const res = await getAllOrdersheet();
+    console.log(res);
+
+    const promises = Object.values(res).map(orderSheetToProps);
+
+    return Promise.all(promises);
 }
