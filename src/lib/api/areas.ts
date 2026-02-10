9004874b4c3aa0d2a6d7cba01ab488d228a1e351
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-10 14:27:49 -0400

    chore: get area from order sheet val

--- src/lib/api/areas.ts
+++ src/lib/api/areas.ts
@@ -1,10 +1,33 @@
-import { OrdersheetsResponse } from "@/types/api.types";
+import { AreaValue, OrdersheetValue } from "@/types/api.types";
 import { apiUrl } from "../../constants/api";
 
-export async function getAllAreas(): Promise<OrdersheetsResponse> {
+export async function getAllAreas(): Promise<AreaValue[]> {
     const res = await fetch(`${apiUrl}/areas`);
     if (!res.ok) {
         throw new Error("Failed to fetch");
     }
     return res.json();
+}
+
+export async function getAreaFromOrderSheetVal(ordersheetVal: OrdersheetValue) {
+    if (!ordersheetVal.checkpad) {
+        return null;
+    }
+
+    const checkpadModel = ordersheetVal.checkpad.model;
+    const checkpadIcon = ordersheetVal.checkpad.modelIcon;
+
+    const areas = await getAllAreas();
+
+    const area = areas.find((area) => {
+        return area.checkpadModels.some(
+            (model) => model.name === checkpadModel && model.icon === checkpadIcon,
+        );
+    });
+
+    if (!area) {
+        return null;
+    }
+
+    return area;
 }
