f4fb20fce3800b82882248de13619d5a66132e6c
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-09 22:59:32 -0400

    chore: new icon preset

--- src/constants/iconPresets.ts
+++ src/constants/iconPresets.ts
@@ -3,7 +3,9 @@ import { SVGProps } from "react";
 export const descriptionIconProps: SVGProps<SVGSVGElement> = {
     width: "10px",
     height: "10px",
-    style: {
-        marginRight: "2px",
-    },
+} as const;
+
+export const headerIconProps: SVGProps<SVGSVGElement> = {
+    width: "14px",
+    height: "15px",
 } as const;
