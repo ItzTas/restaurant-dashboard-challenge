4f6ae52f34a4c1a0eda458408aa4151de1dc5e3a
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-10 15:18:05 -0400

    chore: function removed

--- src/lib/api/areas.ts
+++ src/lib/api/areas.ts
@@ -1,4 +1,4 @@
-import { AreaValue, OrdersheetValue } from "@/types/api.types";
+import { AreaValue } from "@/types/api.types";
 import { apiUrl } from "../../constants/api";
 
 export async function getAllAreas(): Promise<AreaValue[]> {
@@ -7,27 +7,4 @@ export async function getAllAreas(): Promise<AreaValue[]> {
         throw new Error("Failed to fetch");
     }
     return res.json();
-}
-
-export async function getAreaFromOrderSheetVal(ordersheetVal: OrdersheetValue) {
-    if (!ordersheetVal.checkpad) {
-        return null;
-    }
-
-    const checkpadModel = ordersheetVal.checkpad.model;
-    const checkpadIcon = ordersheetVal.checkpad.modelIcon;
-
-    const areas = await getAllAreas();
-
-    const area = areas.find((area) => {
-        return area.checkpadModels.some(
-            (model) => model.name === checkpadModel && model.icon === checkpadIcon,
-        );
-    });
-
-    if (!area) {
-        return null;
-    }
-
-    return area;
 }
