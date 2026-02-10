4840bc7f2055e1b4ed62c4040670fd734c5d5e0b
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-10 10:25:49 -0400

    chore: get ordersheet initialized

---
+++ src/lib/ordersheet.ts
@@ -0,0 +1,7 @@
+import { CardOrdersheetProps } from "@/components/cards/CardOrdersheet";
+
+export async function getDashboardOrdersheets(): Promise<
+    CardOrdersheetProps[]
+> {
+    return [];
+}
