fcbe7811c2a5174576bb87e5a7bf79aa18057165
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-10 18:58:02 -0400

    chore: card mesa list filter

--- src/components/cardLists/DashboardCardMesaList.tsx
+++ src/components/cardLists/DashboardCardMesaList.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useFilterQuery } from "@/features/filters/hooks";
 import CardMesa from "../cards/CardMesa";
 import { CardMesaProps } from "../cards/CardMesa";
 import CardsGridList from "./CardsGridList";
@@ -9,9 +10,23 @@ export interface CardMesaListProps {
 }
 
 export default function CardMesaList({ cards }: CardMesaListProps) {
+    const filterQuery = useFilterQuery().toLowerCase();
+
+    const filteredCards = cards.filter((card) => {
+        const identifier = card.identifier.toLowerCase();
+        const customerName = card.data?.customer.value.toLowerCase() ?? "";
+        const waiterName = card.data?.waiterFullName.toLowerCase() ?? "";
+
+        return (
+            identifier.includes(filterQuery) ||
+            customerName.includes(filterQuery) ||
+            waiterName.includes(filterQuery)
+        );
+    });
+
     return (
         <CardsGridList
-            data={cards}
+            data={filteredCards}
             itemContent={(_, card) => <CardMesa {...card} />}
         />
     );
