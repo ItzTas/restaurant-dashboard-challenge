b101cedc9b5e0e1a410e00810b65007346a0968e
Tales <59647566+ItzTas@users.noreply.github.com>
2026-02-09 22:10:11 -0400

    chore: get all ordersheet by id

--- src/lib/api/ordersheet.ts
+++ src/lib/api/ordersheet.ts
@@ -1,10 +1,33 @@
-import { OrdersheetsResponse } from "@/types/apiResponse.types";
+import {
+    OrdersheetsResponse,
+    OrdersheetValue,
+} from "@/types/apiResponse.types";
 import { apiUrl } from "../../constants/api";
 
 export async function getAllOrdersheet(): Promise<OrdersheetsResponse> {
-    const res = await fetch(`${apiUrl}/ordersheet`);
+    const res = await fetch(`${apiUrl}/ordersheets`);
     if (!res.ok) {
+        console.log(res);
         throw new Error("Failed to fetch");
     }
     return res.json();
+}
+
+export async function getOrdersheetsRecordByIds(
+    ids: number[],
+): Promise<Record<string, OrdersheetValue>> {
+    if (ids.length === 0) return {};
+
+    const allOrdersheets = await getAllOrdersheet();
+    const idsSet = new Set(ids);
+
+    const output: Record<string, OrdersheetValue> = {};
+
+    for (const ordersheet of Object.values(allOrdersheets)) {
+        if (!idsSet.has(ordersheet.id)) continue;
+
+        output[ordersheet.id] = ordersheet;
+    }
+
+    return output;
 }
